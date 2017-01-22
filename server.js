d7110839c99490b8ce1b2f753ecc2f24146668be
Tine Jozelj <mentos1386@outlook.com>
2017-01-22 20:12:31 +0100

    Oh, you can verify secret

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const express    = require('express');
 const server     = express();
 const bodyParser = require('body-parser');
 const _          = require('lodash');
+const crypto     = require('crypto');
+const bufferEq   = require('buffer-equal-constant-time');
 
 // Use bodyParser
 server.use(bodyParser.json({ type : 'application/json' }));
 
 // Trust NGINX proxy
-server.set('trust proxy', 'loopback');
+server.set('trust proxy');
+
+function signData( secret, data ) {
+  return 'sha1=' + crypto.createHmac('sha1', secret).update(data).digest('hex');
+}
 
 // Load all hooks
 const hooks = require('require-all')({
@@ -29,8 +35,16 @@ server.post('/:webhook', ( req, res ) => {
   }
 
   const payload        = req.body;
+  const providedSecret = req.headers[ 'x-hub-signature' ] || '';
   const hook           = _.find(secrets, { name : webhook });
 
+  // Verify secret, if wrong, return 404
+  if ( !bufferEq(new Buffer(providedSecret), new Buffer(signData(hook.secret, JSON.stringify(payload)))) ) {
+    console.log(`ACCESS : ${req.ip} : Used wrong secret`);
+    return res.status(404).send();
+  }
+
+  // Request verified, proceed
   hook.run(payload);
 
   // Return 200
