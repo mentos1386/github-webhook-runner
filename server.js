5a3d38346052f78680dd012c98fdddab462a9ca2
Tine Jozelj <mentos1386@outlook.com>
2017-01-22 20:49:03 +0100

    Fixes

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ server.post('/:webhook', ( req, res ) => {
   }
 
   // Request verified, proceed
-  webhook[ hook ].run(payload);
+  hooks[ hook ].run(payload);
 
   // Return 200
   res.send(200)
