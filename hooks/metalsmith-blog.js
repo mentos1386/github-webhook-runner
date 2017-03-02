aef63e5616517e6222245830aa84506b4476f03c
Tine Jozelj <mentos1386@outlook.com>
2017-03-02 20:31:18 +0100

    Implement metalsmith-blog tor building

--- hooks/metalsmith-blog.js
+++ hooks/metalsmith-blog.js
@@ -1,5 +1,9 @@
-const exec       = require('child_process').exec;
-const workingDir = '/var/www/metalsmith-blog/';
+const exec             = require('child_process').exec;
+const workingDirNormal = '/var/www/metalsmith-blog/';
+const workingDirTor    = '/var/www/tor/metalsmith-blog/';
+
+const torEnv       = process.env;
+torEnv.BLOG_DOMAIN = 'http://o7wdpwqkabsjqjby.onion/';
 
 /**
  * Run hook
@@ -8,11 +12,21 @@ const workingDir = '/var/www/metalsmith-blog/';
 exports.run = ( payload ) => {
   console.log('Executing : metalsmith-blog : Build');
 
-  return new Promise(( resolve, reject ) => {
-    exec(`cd ${workingDir} && git pull && npm install && npm run build`, err => {
-      if ( err ) return reject(err);
-      return resolve()
+  Promise.all([
+    // Normal
+    new Promise(( resolve, reject ) => {
+      exec(`cd ${workingDirNormal} && git pull && npm install && npm run build`, err => {
+        if ( err ) return reject(err);
+        return resolve()
+      })
+    }),
+    // Tor
+    new Promise(( resolve, reject ) => {
+      exec(`cd ${workingDirTor} && git pull && npm install && npm run build`, { env : torEnv }, err => {
+        if ( err ) return reject(err);
+        return resolve()
+      })
     })
-  })
-    .catch(err => console.log('ERROR : metalsmith-blog : Build'))
+  ])
+  .catch(err => console.log('ERROR : metalsmith-blog : Build'))
 };
